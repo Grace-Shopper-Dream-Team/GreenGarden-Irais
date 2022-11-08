8348c3a288c80fc41735b52a4b0a38f8c63b345f
Lu Miao <miaolphoebe@gmail.com>
2022-11-08 18:05:46 -0500

    update single product view & delete all products route

--- server/api/products.js
+++ server/api/products.js
@@ -3,15 +3,6 @@ const {
   models: { Product },
 } = require("../db/index");
 
-router.get("/", async (req, res, next) => {
-  try {
-    const allProducts = await Product.findAll();
-    res.json(allProducts);
-  } catch (error) {
-    next(error);
-  }
-});
-
 router.get("/:productId", async (req, res, next) => {
   try {
     const product = await Product.findByPk(req.params.productId);
