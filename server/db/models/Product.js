430ec99e5ef3862005665db02d9f589a4af7ae94
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2022-11-08 16:08:44 -0500

    added Product model & seeded 10 products to seed.js

---
+++ server/db/models/Product.js
@@ -0,0 +1,22 @@
+const { Sequelize, DataTypes } = require('sequelize');
+const db = require('../db');
+
+module.exports = db.define('product', {
+  name: {
+    type: DataTypes.STRING,
+    unique: true,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false,
+  },
+  desc: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
