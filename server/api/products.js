b5ddc96a67dcfe9524dd87a608419de81d82d990
MichelleMullane <106880578+MichelleMullane@users.noreply.github.com>
2022-11-09 10:39:13 -0500

    all products view

---
+++ server/api/products.js
@@ -0,0 +1,15 @@
+const router = require('express').Router();
+const {
+  models: { Product },
+} = require('../db');
+
+router.get('/', async (req, res, next) => {
+  try {
+    const products = await Product.findAll();
+    res.send(products);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
