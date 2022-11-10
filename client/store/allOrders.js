d5e7e2d0a1e7657a610700ce39b85ae64161b5c8
nadiakhristean <nadia.khristean@gmail.com>
2022-11-09 21:36:22 -0500

    Worked on allOrders and currentOrder inside store more

--- client/store/allOrders.js
+++ client/store/allOrders.js
@@ -6,13 +6,13 @@ import { getCurrentOrder } from "./currentOrder";
 
 //Action Types
 const GET_ALL_ORDERS = "GET_ALL_ORDERS";
-const REMOVE_ORDER = "REMOVE_ORDER";
+// const REMOVE_ORDER = "REMOVE_ORDER";
 // const ADD_ORDER = "ADD_ORDER";
 // const UPDATE_ORDER = "UPDATE_ORDER";
 
 //Action Creators
-const getAllOrders = (orders) => ({ type: GET_ALL_ORDERS, orders });
-const removeOrder = (id) => ({ type: REMOVE_ORDER, id });
+const _getAllOrders = (orders) => ({ type: GET_ALL_ORDERS, orders });
+// const _removeOrder = (id) => ({ type: REMOVE_ORDER, id });
 // const addOrder = (order) => ({ type: ADD_ORDER, order });
 // const updateOrder = (order) => ({ type: UPDATE_ORDER, order });
 
@@ -21,8 +21,8 @@ export default (orders = [], action) => {
   switch (action.type) {
     case GET_ALL_ORDERS:
       return action.orders;
-    case REMOVE_ORDER:
-      return orders.filter((order) => order.id !== action.orderId);
+    // case REMOVE_ORDER:
+    //   return orders.filter((order) => order.id !== action.orderId);
     //   case ADD_ORDER:
     //     return [...orders, action.order]
     //   case UPDATE_ORDER:
@@ -33,6 +33,24 @@ export default (orders = [], action) => {
 };
 
 //Thunks
+export const getAllOrders = (orders) => (dispatch) => {
+  return async (dispatch) => {
+    const { data: lineItem } = await axios.get("/api/orders/:orderId");
+    dispatch(_getAllOrders(orders));
+  };
+};
+
+//~~~~> NOT SURE WHAT TO DO HERE
+export const deleteLineItem = (orderId, lineItemId) => (dispatch) => {
+  return async (dispatch) => {
+    const { data: orders } = await axios.get("/api/orders/:orderId");
+    dispatch(_deleteLineItem(orders));
+  };
+};
+
+//OTHER TEAM REFERENCE:
+
+/*
 export const fetchOrders = () => (dispatch) => {
   axios
     .get("/api/orders")
@@ -49,3 +67,4 @@ export const deleteLineItem = (orderId, lineItemId) => (dispatch) => {
       console.error(`error deleting line item id ${lineItemId}`, err)
     );
 };
+*/
--- client/store/currentOrder.js
+++ client/store/currentOrder.js
@@ -4,10 +4,15 @@ import axios from "axios";
 
 // Action Types
 const GET_CURRENT_ORDER = "GET_CURRENT_ORDER";
+const DELETE_LINE_ITEM = "DELETE_LINE_ITEM";
 
 // Action Creators
-const _getCurrentOrder = (order) => {
-  return { type: GET_CURRENT_ORDER, order };
+const _getCurrentOrder = (orderId) => {
+  return { type: GET_CURRENT_ORDER, orderId };
+};
+
+const _deleteLineItem = (lineItemId) => {
+  return { type: DELETE_LINE_ITEM, lineItemId };
 };
 
 // Reducer
@@ -15,17 +20,27 @@ export default (currentOrder = {}, action) => {
   switch (action.type) {
     case GET_CURRENT_ORDER:
       return action.order;
+    case DELETE_LINE_ITEM:
+      return state.filter((lineItem) => lineItem.id !== action.lineItem.id);
     default:
       return currentOrder;
   }
 };
 
 // Thunks
-//~~~~> Need to use the Order Id more here?
+//~~~~> Need to use the Order Id here?
 export const getCurrentOrder = (id) => (dispatch) => {
   return async (dispatch) => {
     const { data: orders } = await axios.get("/api/orders/:orderId");
     dispatch(_getCurrentOrder(orders));
+  };
+};
+
+export const deleteLineItem = (id) => (dispatch) => {
+  return async (dispatch) => {
+    const { data: orders } = await axios.delete(`/api/orders/${id}`);
+    dispatch(_deleteLineItem(todo));
+    history.push("/");
   };
 };
 
