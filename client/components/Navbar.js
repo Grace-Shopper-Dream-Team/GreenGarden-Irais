7f6f0625305daa2a59bbd1d159875be4c2f5a815
nadiakhristean <nadia.khristean@gmail.com>
2022-11-15 03:30:22 -0500

    Recent updates

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -5,7 +5,7 @@ import { logout } from "../store";
 
 const Navbar = ({ handleClick, isLoggedIn }) => (
   <div>
-    <h1 className="logo">
+    <h1>
       <Link to="/home">Green Garden</Link>
     </h1>
     <nav>
