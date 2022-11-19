8dfe253e1f0a312f90dec859d03b633de6ed1362
Lu Miao <miaolphoebe@gmail.com>
2022-11-18 19:19:30 -0500

    try mysql

--- server/db/db.js
+++ server/db/db.js
@@ -12,12 +12,6 @@ if (process.env.LOGGING === "true") {
   delete config.logging;
 }
 
-const db = new Sequelize(
-  // `postgres://gardeners:o5fu6RIK7bIcrvBK2HMQi5mVh1Pnu4lX@dpg-cdpvmepa6gdl61gqibs0-a/green_garden`,
-  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
-  config
-);
-
 //https://stackoverflow.com/questions/61254851/heroku-postgres-sequelize-no-pg-hba-conf-entry-for-host
 if (process.env.DATABASE_URL) {
   config.dialectOptions = {
@@ -25,12 +19,12 @@ if (process.env.DATABASE_URL) {
       rejectUnauthorized: false,
     },
   };
-  try {
-    await db.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
 }
+
+const db = new Sequelize(
+  // `postgres://gardeners:o5fu6RIK7bIcrvBK2HMQi5mVh1Pnu4lX@dpg-cdpvmepa6gdl61gqibs0-a/green_garden`,
+  process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
+  config
+);
 
 module.exports = db;
